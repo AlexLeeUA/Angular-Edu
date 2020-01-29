c5f022e0cd21f9263afe9452177b94f8a382ef1b
Alexandr <a.litkevich@smissltd.com>
2020-01-29 13:04:51 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  title = 'Initial';
+
+  onInput = (event:any) => {
+    this.title = event.target.value;
+  }
+}
---
+++ src/app/post4/post4.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-post4',
+    template: `
+        <div class="post4">Lower Marion High School</div>
+    `,
+    styles: [`
+        .post4 {
+            font-size: 20px;
+            color: yellow;
+            font-weight: bold;
+        }
+    `]
+})
+
+export class Post4Component {}
