72691ea675dd6c84af06970fc1f8a6c8976dbaac
Alexander <a.litkevich@smissltd.com>
2020-02-10 11:22:45 +0200

    Added working changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
+export interface Post {
+  title: string
+  text: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  title = 'Initial';
+  p: Promise<string> = new Promise<string>(resolve => setTimeout(() => resolve('Promise resolved'), 4000));
 
-  onInput = (event:any) => {
-    this.title = event.target.value;
+  date$: Observable<Date> = new Observable(obs => {setInterval(() => {obs.next(new Date())}, 1000)})
+
+  date: Date
+
+  ngOnInit(): void {
+    this.date$.subscribe(date => this.date = date)
   }
+
 }
